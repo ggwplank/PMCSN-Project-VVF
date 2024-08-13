c758e333b27f1c140500de5986c3d1caf619db43
Simone <simoneniro17@gmail.com>
2024-08-13 11:45:49 +0200

    added comments to event.py and server.py

--- simulation/event.py
+++ simulation/event.py
@@ -1,11 +1,11 @@
 class Event:
     def __init__(self, current_time, arrival_time, hub_completion, red_completion, yellow_completion, green_completion,
                  white_completion, color=None):
-        self.current = current_time
-        self.arrival = arrival_time
-        self.color = color
-        self.hub_completion = hub_completion
-        self.red_completion = red_completion
-        self.yellow_completion = yellow_completion
-        self.green_completion = green_completion
-        self.white_completion = white_completion
+        self.current = current_time # tempo attuale dell'evento
+        self.arrival = arrival_time # tempo del prossimo arrivo nel sistema
+        self.color = color  # colore associato all'evento, se applicabile (tipo di richiesta)
+        self.hub_completion = hub_completion    # tempo di completamento del prossimo job nel centralino
+        self.red_completion = red_completion    # tempo di completamento del prossimo job in emergenza massima
+        self.yellow_completion = yellow_completion  # tempo di completamento del prossimo job in emergenza alta
+        self.green_completion = green_completion    # tempo di completamento del prossimo job in emergenza media
+        self.white_completion = white_completion    # tempo di completamento del prossimo job in emergenza bassa
--- simulation/server.py
+++ simulation/server.py
@@ -4,6 +4,6 @@ from utils.constants import INF
 class Server:
     def __init__(self):
         self.service_time = INF  # il server non sta facendo nulla
-        self.occupied = False
-        self.color = None
+        self.occupied = False  # flag che indica se il server è attualmente occupato
+        self.color = None  # colore associato al server
         self.start_time = None  # per tracciare l'inizio del servizio
