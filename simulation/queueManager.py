5f58a6a93ee2c10c6e0bffceb7438bf06ec6024a
Simone <simoneniro17@gmail.com>
2024-08-16 14:39:29 +0200

    refactor and comments

--- simulation/queueManager.py
+++ simulation/queueManager.py
@@ -1,35 +1,40 @@
+from collections import deque
+
+
 class QueueManager:
     def __init__(self):
+        # inizializzazione code per colori e hub
         self.queues = {
-            "hub": [],
-            "red": [],
-            "yellow": [],
-            "green": [],
-            "white": []
+            "hub": deque(), #deque per ottimizzare le operazioni di append e pop
+            "red": deque(),
+            "yellow": deque(),
+            "green": deque(),
+            "white": deque()
         }
 
-    def add_job_to_queue(self, color, job_time):
-        if color in self.queues:
-            self.queues[color].append(job_time)
-        else:
-            print(f"Error: Invalid color '{color}'")
+    def validate_color(self, color):
+        """Verifica se il colore specificato è valido."""
+        if color not in self.queues:
+            raise ValueError(f"Error: Invalid color '{color}'")
+        return True
 
-    def get_next_job_from_queue(self, color):
-        if color in self.queues and self.queues[color]:
-            return self.queues[color].pop(0)
-        else:
-            return None
+    def add_to_queue(self, color, job_time):
+        """Aggiunge un job alla coda specificata dal colore."""
+        if self.validate_color(color):
+            self.queues[color].append(job_time)
+
+    def get_from_queue(self, color):
+        """Rimuove e restituisce il primo job dalla coda specificata dal colore."""
+        if self.validate_color(color) and self.queues[color]:
+            return self.queues[color].popleft()
+        return None
 
     def is_queue_empty(self, color):
-        if color in self.queues:
+        """Verifica se la coda specificata dal colore è vuota."""
+        if self.validate_color(color):
             return len(self.queues[color]) == 0
-        else:
-            print(f"Error: Invalid color '{color}'")
-            return True
 
     def get_queue_length(self, color):
-        if color in self.queues:
+        """Restituisce la lunghezza della coda specificata dal colore."""
+        if self.validate_color(color):
             return len(self.queues[color])
-        else:
-            print(f"Error: Invalid color '{color}'")
-            return 0
