fb12f191a3dd32436b8ebc391328d0fc0016e8d0
ggwplank <brunori.lorenzo2001@gmail.com>
2024-08-23 12:06:06 +0200

    aggiornata prova grafici

--- graphs/prova.py
+++ graphs/prova.py
@@ -11,15 +11,20 @@ data = pd.read_csv(csv_file_path)
 
 # Estrae le colonne necessarie per il grafico
 simulation_runs = data['Simulation']
-mean_service_hub_time = data['mean_service_hub_time']
+mean_service_hub_time = data['mean_service_red_time']
 
-plt.figure(figsize=(12, 8))
-plt.plot(simulation_runs, mean_service_hub_time, marker='o', linestyle='-', color='b')
+sample_rate = 16  # Mostra solo ogni 10 run
+sampled_runs = simulation_runs[::sample_rate]
+sampled_mean_service_hub_time = mean_service_hub_time[::sample_rate]
+
+plt.figure(figsize=(16, 8))
+plt.plot(sampled_runs, sampled_mean_service_hub_time, marker='o', linestyle='-', color='b')
 plt.xlabel('Simulation Run')
 plt.ylabel('Mean Service Time at Hub')
-plt.title('Mean Service Time at Hub per Simulation Run')
+plt.title('Sampled Mean Service Time at Hub per Simulation Run')
 plt.grid(True)
 plt.xticks(rotation=45)
-plt.tight_layout()  # Adatta il layout per evitare il taglio dei testi
-plt.savefig(os.path.join(OUTPUTS_DIR, 'mean_service_time_at_hub.png'))
+plt.tight_layout()
+plt.savefig(os.path.join(OUTPUTS_DIR, 'sampled_mean_service_time_at_hub.png'))
 plt.show()
+
